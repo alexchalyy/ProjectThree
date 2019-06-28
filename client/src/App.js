f328058d67915863c2b87d13b0788781602a8dae
alex chalyy <alexchalyy@gmail.com>
2019-06-28 11:56:06 -0400

    handleinput change is not working after its moved to app.js from enter.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,143 +10,183 @@ import Order from "./pages/Orders";
 import Account from "./pages/Account";
 import React, { Component } from "react";
 import { Redirect } from 'react-router-dom';
+import Axios from 'axios';
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-        email: "",
-        password: "",
-        hiddenPassword: [],
-        redirect: false,
-        credentials: [],
-        modal: false,
-        nestedModal: false,
-        closeAll: false,
-        maskPassword: ""
+      email: "",
+      password: "",
+      hiddenPassword: [],
+      redirect: false,
+      credentials: [],
+      modal: false,
+      nestedModal: false,
+      closeAll: false,
+      maskPassword: ""
     };
 
     this.toggle = this.toggle.bind(this);
     this.toggleNested = this.toggleNested.bind(this);
     this.toggleAll = this.toggleAll.bind(this);
-}
+  }
 
-toggle() {
-  this.setState(prevState => ({
+  toggle() {
+    this.setState(prevState => ({
       modal: !prevState.modal
-  }));
-}
+    }));
+  }
 
-toggleNested() {
-  this.setState({
+  toggleNested() {
+    this.setState({
       nestedModal: !this.state.nestedModal,
       closeAll: false
-  });
-}
+    });
+  }
 
-toggleAll() {
-  this.setState({
+  toggleAll() {
+    this.setState({
       nestedModal: !this.state.nestedModal,
       closeAll: true
-  });
-}
-
-setRedirect = () => {
-  this.setState({
-      redirect: true
-  })
-}
-
-renderRedirect = () => {
-  console.log("redirect works!");
-  if (this.state.redirect) {
-      return <Redirect to='/sale' />
+    });
   }
-}
 
-handleFormSubmit = event => {
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    })
+  }
 
-  //var users = []; // array of users (including username and password), is populated every time user clicks log in button for input validation
+  renderRedirect = () => {
+    console.log("redirect works!");
+    if (this.state.redirect) {
+      return <Redirect to='/sale' />
+    }
+  }
 
-  const user = {
+  handleFormSubmit = event => {
+
+    //var users = []; // array of users (including username and password), is populated every time user clicks log in button for input validation
+
+    const user = {
 
       email: this.state.email,
       password: this.state.password,
       match: false
 
-  }
+    }
 
-  console.log("credentials on hitting submit button");
-  console.log(this.state.credentials);
-  console.log("the first e-mail of credentials");
-  console.log(this.state.credentials[0]);
-  console.log("submit!");
-  console.log("email: " + this.state.email);
-  console.log("password: " + this.state.password);
-  if (!this.ValidateEmail() || !this.state.password) {
+    console.log("credentials on hitting submit button");
+    console.log(this.state.credentials);
+    console.log("the first e-mail of credentials");
+    console.log(this.state.credentials[0]);
+    console.log("submit!");
+    console.log("email: " + this.state.email);
+    console.log("password: " + this.state.password);
+    if (!this.ValidateEmail() || !this.state.password) {
       console.log("incorrect login");
-  } else {
+    } else {
       for (var c = 0; c < this.state.credentials.length; c = c + 2) {
-          console.log("e-mail #" + c + ": " + this.state.credentials[c]);
-          console.log("password #" + c + ": " + this.state.credentials[c + 1]);
-          if (this.state.email == this.state.credentials[c] && this.state.password == this.state.credentials[c + 1]) {
-              console.log("entered e-mail: " + this.state.email);
-              console.log("entered password: " + this.state.password);
-              console.log("should redirect to next page!");
-              this.setRedirect();
-              this.renderRedirect();
-          }
+        console.log("e-mail #" + c + ": " + this.state.credentials[c]);
+        console.log("password #" + c + ": " + this.state.credentials[c + 1]);
+        if (this.state.email == this.state.credentials[c] && this.state.password == this.state.credentials[c + 1]) {
+          console.log("entered e-mail: " + this.state.email);
+          console.log("entered password: " + this.state.password);
+          console.log("should redirect to next page!");
+          this.setRedirect();
+          this.renderRedirect();
+        }
       }
       console.log("this is credentials array")
       console.log("woof e-mail: " + this.state.credentials[10]);
       console.log("woof password: " + this.state.credentials[11]);
       console.log("b4 toggle");
       if (!this.state.redirect) {
-          this.toggleNested();
+        this.toggleNested();
       }
       console.log("after toggle");
+    }
   }
-}
 
-ValidateEmail() {
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  ValidateEmail() {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  return re.test(String(this.state.email).toLowerCase());
-}
+    return re.test(String(this.state.email).toLowerCase());
+  }
 
-render() {
-  return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={LogIn} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/account" component={Account} />
-          <Route exact path="/login" 
-          component = {Enter}
-          component={() => <Enter redirect={this.redirect}/>}
-          component={() => <Enter toggle={this.toggle}/>}
-          component={() => <Enter toggleAll={this.toggleAll}/>}
-          component={() => <Enter toggleNested={this.toggleNested}/>}
-          component={() => <Enter email={this.state.email}/>}
-          component={() => <Enter password={this.state.password}/>}
-          component={() => <Enter hiddenPassword={this.state.hiddenPassword}/>}
-          component={() => <Enter redirect={this.state.redirect}/>}
-          component={() => <Enter credentials={this.state.credentials}/>}
-          component={() => <Enter modal={this.state.modal}/>}
-          component={() => <Enter nestedModal={this.state.nestedModal}/>}
-          component={() => <Enter closeAll={this.state.closeAll}/>}
-          component={() => <Enter handleFormSubmit={this.handleFormSubmit}/>}
-          />
-          <Route exect path="/forgot" component={Forgot} />
-          <Route exact path = "/sale" component = {Sale} />
-          <Route exact path = "/orders" component = {Order} />
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
-    </Router>
-  );
-}
+  setCredentials = (cred) => {
+    this.setState({
+      credentials: cred
+    })
+  }
+
+  handleInputChange = event => {
+
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+    console.log("value is " + value);
+
+    var credentials = [];
+    Axios.get('/api/allusers')
+      .then(function (response) {
+        console.log("inside axios");
+        console.log(response);
+        for (var c = 0; c < response.data.length; c++) {
+          credentials.push(response.data[c].email);
+          credentials.push(response.data[c].password);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    this.setCredentials(credentials);
+    console.log("Credentials: ");
+    //console.log(this.state.credentials);
+  }
+
+  componentDidUpdate() {
+    console.log("updated: ", this.state);
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={LogIn} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/account" component={Account} />
+            <Route exact path="/login"
+              component={Enter}
+              component={() => <Enter redirect={this.redirect} />}
+              component={() => <Enter toggle={this.toggle} />}
+              component={() => <Enter toggleAll={this.toggleAll} />}
+              component={() => <Enter toggleNested={this.toggleNested} />}
+              component={() => <Enter email={this.state.email} />}
+              component={() => <Enter password={this.state.password} />}
+              component={() => <Enter hiddenPassword={this.state.hiddenPassword} />}
+              component={() => <Enter redirect={this.state.redirect} />}
+              component={() => <Enter credentials={this.state.credentials} />}
+              component={() => <Enter modal={this.state.modal} />}
+              component={() => <Enter nestedModal={this.state.nestedModal} />}
+              component={() => <Enter closeAll={this.state.closeAll} />}
+              component={() => <Enter handleFormSubmit={this.handleFormSubmit} />}
+              component={() => <Enter handleInputChange={this.handleInputChange} />}
+              component={() => <Enter setCredentials={this.setCredentials} />}
+              component={() => <Enter componentDidUpdate={this.componentDidUpdate} />}
+            />
+            <Route exect path="/forgot" component={Forgot} />
+            <Route exact path="/sale" component={Sale} />
+            <Route exact path="/orders" component={Order} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- client/src/pages/Enter.js
+++ client/src/pages/Enter.js
@@ -12,42 +12,6 @@ import Axios from 'axios';
 
 class Enter extends Component {
 
-    setCredentials = (cred) => {
-        this.setState({
-            credentials: cred
-        })
-    }
-
-    handleInputChange = event => {
-
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value
-        });
-        console.log("value is " + value);
-        
-        var credentials = [];
-        Axios.get('/api/allusers')
-            .then(function (response) {
-                console.log("inside axios");
-                console.log(response);
-                for (var c = 0; c < response.data.length; c++) {
-                    credentials.push(response.data[c].email);
-                    credentials.push(response.data[c].password);
-                }
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-        this.setCredentials(credentials);
-        console.log("Credentials: ");
-        //console.log(this.state.credentials);
-    }
-
-    componentDidUpdate() {
-        console.log("updated: ", this.state);
-    }
-
     render() {
         return (
             <div className="App">
@@ -55,24 +19,24 @@ class Enter extends Component {
                 {this.props.redirect}
                 <div className='container'>
                     <AvForm>
-                        <AvField name="email" label="Email" type="email" onChange={this.handleInputChange} validate={{
+                        <AvField name="email" label="Email" type="email" onChange={this.props.handleInputChange} validate={{
                             email: { value: true, errorMessage: 'Please enter valid e-mail' },
                             required: { value: true, errorMessage: 'Please enter e-mail' }
                         }} />
-                        <AvField name="password" type="password" label="Password" value={this.handleInputChange} onChange={this.handleInputChange} validate={{
+                        <AvField name="password" type="password" label="Password" value={this.props.handleInputChange} onChange={this.props.handleInputChange} validate={{
                             required: { value: true, errorMessage: 'Please enter password' },
                             pattern: { value: '^[A-Za-z0-9]+$', errorMessage: 'Your password must be composed only with letter and numbers' },
                             minLength: { value: 6, errorMessage: 'Your password must be between 6 and 16 characters' },
                             maxLength: { value: 16, errorMessage: 'Your password must be between 6 and 16 characters' }
                         }} />
-                        <Button color="primary" onClick={this.handleFormSubmit}>Submit</Button>
+                        <Button color="primary" onClick={this.props.handleFormSubmit}>Submit</Button>
                     </AvForm>
                 </div>
                 <div>
-                    <Modal isOpen={this.props.nestedModal} toggle={this.toggleNested} onClosed={this.props.closeAll ? this.toggle : undefined}>
+                    <Modal isOpen={this.props.nestedModal} toggle={this.props.toggleNested} onClosed={this.props.closeAll ? this.props.toggle : undefined}>
                         <ModalHeader>User not registered</ModalHeader>
                         <ModalFooter>
-                            <Button color="secondary" onClick={this.toggleAll}>Close</Button>
+                            <Button color="secondary" onClick={this.props.toggleAll}>Close</Button>
                         </ModalFooter>
                     </Modal>
                 </div>
